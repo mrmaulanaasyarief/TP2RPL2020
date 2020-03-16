367f9918e63e841b60cd05e0fdfc5553e35d996f
mrmaulanaasyarief <mr.maulanaasyarief@gmail.com>
2020-03-16 16:59:07 +0700

    first commit

--- app.js
+++ app.js
@@ -7,27 +7,34 @@ const DB_USER = [
 
 const inputUsername = document.querySelector("#username");
 const inputPassword = document.querySelector("#password");
+const inputCPassword = document.querySelector("#cPassword");
 const buttonSubmit = document.querySelector("#submitForm");
+const warningMsg = document.querySelector(".warning");
 
 buttonSubmit.addEventListener('click', (e) => {
     e.preventDefault();
 
     const valueInputUsername = inputUsername.value;
     const valueInputPassword = inputPassword.value;
+    const valueInputCPassword = inputCPassword.value;
 
-    let flag = 0;
-    DB_USER.map((data) => {
-        if (data.username === valueInputUsername && data.password === valueInputPassword){
-            flag = 1;
+    if (valueInputPassword === valueInputCPassword) {
+        //Hide Warning Text
+        warningMsg.classList.add("warning");
+
+        const data = {
+            username: valueInputUsername,
+            password: valueInputPassword
         }
-    })
 
-    const warningMsg = document.querySelector(".warning");
+        DB_USER.push(data);
 
-    if (flag === 0) {
-        warningMsg.className = "text-danger";
-    } else {
-        alert("Selamat anda masuk ke home");
+        // Response Success
+        console.log("Registrasi Berhasil, Terimakasih!");
+        console.log(DB_USER);
+    }else{
+        // Respons When Password is Different
+        warningMsg.classList.remove("warning");
     }
 
 })
